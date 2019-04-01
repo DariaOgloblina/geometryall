2dc2a5a68d08250a0959069860e8ea8db87e2bea
Daria Ogloblina <ogloblik@yandex.ru>
2019-04-01 18:19:50 +0700

    Created header file with #include guard

---
+++ foo.h
@@ -0,0 +1,7 @@
+#ifdef H_FOO
+#define H_FOO
+float plo(char figure[10], float x1, float y1, float x2, float y2, float x3,
+	  float y3, float x4, float y4);
+float per(char figure[10], float x1, float y1, float x2, float y2, float x3,
+	  float y3, float x4, float y4);
+#endif
