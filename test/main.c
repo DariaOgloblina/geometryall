43c0384ab2e5ca868f3861f7b8125b66db28d6d3
Daria Ogloblina <ogloblik@yandex.ru>
2019-04-16 21:07:59 +0700

    Test to calculate the area of a triangle

--- test/main.c
+++ test/main.c
@@ -23,4 +23,22 @@ CTEST(area_test, result_test)
     const float expected = 63.615375;
     ASSERT_EQUAL(expected, result);
 }
+CTEST(area_testt, resultt_test)
+{
+    // Given
+    char figure[10]="triangle";
+    const float x1=12;
+    const float y1=13;
+    const float x2=4;
+    const float y2=1;
+    const float x3=7;
+    const float y3=8;
+    // When
+    const float result = plo(figure,x1,y1,x2,y2,x3,y3,x1,y1);
+
+    // Then
+    const float expected = 10;
+    ASSERT_EQUAL(expected, result);
+}
+
 
